afb6d18f6294532876e984a19efa4e5e3d0a75ab
beltagy <abdo.elbeltagy@tum.de>
2026-08-17 22:41:04 +0200

    fix bug and add spring-boot-test-dependency

--- src/main/java/com/Beltagy/documents/authorization/OpaClient.java
+++ src/main/java/com/Beltagy/documents/authorization/OpaClient.java
@@ -3,6 +3,7 @@ package com.Beltagy.documents.authorization;
 import com.Beltagy.documents.OpaInput;
 import com.Beltagy.documents.OpaRequest;
 import com.Beltagy.documents.OpaResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -13,15 +14,9 @@ import org.springframework.web.client.RestClientException;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class OpaClient {
     private final RestClient restClient;
-
-    public OpaClient(RestClient.Builder restClientBuilder,
-                     OpaProperties properties) {
-        this.restClient = restClientBuilder
-                .baseUrl(properties.baseUrl())
-                .build();
-    }
 
     public boolean isAllowed(String role, String method, String path) {
         OpaInput input = new OpaInput(role, method, path);
--- src/main/java/com/Beltagy/documents/configurations/OpaConfiguration.java
+++ src/main/java/com/Beltagy/documents/configurations/OpaConfiguration.java
@@ -2,9 +2,20 @@ package com.Beltagy.documents.configurations;
 
 import com.Beltagy.documents.authorization.OpaProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
 
 @Configuration
 @EnableConfigurationProperties(OpaProperties.class)
-public record OpaConfiguration() {
+public class OpaConfiguration {
+    @Bean
+    public RestClient OpaClient(
+            RestClient.Builder restClientBuilder,
+            OpaProperties properties) {
+
+        return restClientBuilder
+                .baseUrl(properties.baseUrl())
+                .build();
+    }
 }
